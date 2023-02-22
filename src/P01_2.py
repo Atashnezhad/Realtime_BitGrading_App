1862de68bfb68b1ff21b5f953288a294a8a9c764
atashnezhad <atashnezhad1@gmail.com>
2023-02-22 15:55:46 -0600

    automated git @ Wed Feb 22 15:55:46 CST 2023

--- src/P01_2.py
+++ src/P01_2.py
@@ -16,40 +16,123 @@ class GenerateData:
         curr_dt: datetime = datetime.now()
         self.curr_ts: int = int(round(curr_dt.timestamp()))
 
-    def generate_records(self, **kwargs) -> List[Dict[str, Any]]:
+    def _generate_records(self, **kwargs) -> List[Dict[str, Any]]:
+        """
+        Generate records for the project.
+        :param kwargs:
+        mds is measured depth in meters, wob is weight on bit in lbf,
+        rpm is revolutions per minute, rop is rate of penetration in ft per minute.
+        :return:
+        """
         number_of_datapoints: int = kwargs.get("number_of_datapoints", 1000)
+        mds_min: float = kwargs.get("mds_min", 0)
+        mds_max: float = kwargs.get("mds_max", 10_000)
         wob_min: float = kwargs.get("wob_min", 0)
         wob_max: float = kwargs.get("wob_max", 50_000)
         rpm_min: float = kwargs.get("rpm_min", 0)
         rpm_max: float = kwargs.get("rpm_max", 350)
         rop_min: float = kwargs.get("rop_min", 0)
         rop_max: float = kwargs.get("rop_max", 300)
-        wobs: np.ndarray = np.random.uniform(wob_min, wob_max, number_of_datapoints)
-        rpms: np.ndarray = np.random.uniform(rpm_min, rpm_max, number_of_datapoints)
-        rops: np.ndarray = np.random.uniform(rop_min, rop_max, number_of_datapoints)
-        timestamps: List[int] = [self.curr_ts + i for i in range(1, number_of_datapoints)]
-        ids: List[str] = [str(uuid.uuid4()) for _ in range(1, number_of_datapoints)]
+
+        drill_string_id: str = kwargs.get("drill_string_id", "ds_1")
+
+        # mds is increased each data point.
+        mds: np.ndarray = np.linspace(mds_min, mds_max, number_of_datapoints + 1)
+        wobs: np.ndarray = np.random.uniform(wob_min, wob_max, number_of_datapoints + 1)
+        rpms: np.ndarray = np.random.uniform(rpm_min, rpm_max, number_of_datapoints + 1)
+        rops: np.ndarray = np.random.uniform(rop_min, rop_max, number_of_datapoints + 1)
+        timestamps: List[int] = [self.curr_ts + i for i in range(1, number_of_datapoints + 1)]
+        ids: List[str] = [str(uuid.uuid4()) for _ in range(1, number_of_datapoints + 1)]
         records: List[Dict[str, Any]] = [
             {
                 "id": id,
                 "timestamp": ts,
                 "provider": "provider_name",
-                "data": {"wob": round(wob, 3), "rpm": round(rpm, 3), "rop": round(rop, 3)},
+                "drill_string_id": drill_string_id,
+                "data": {"md": round(md, 3), "wob": round(wob, 3), "rpm": round(rpm, 3), "rop": round(rop, 3)},
             }
-            for id, ts, wob, rpm, rop in zip(ids, timestamps, wobs, rpms, rops)
+            for id, ts, md, wob, rpm, rop in zip(ids, timestamps, mds, wobs, rpms, rops)
         ]
         return records
 
-    def save_data(self, **kwargs) -> None:
+    def generate_records_and_save_data(self, **kwargs) -> None:
         _path = kwargs.get("path", Path(__file__).parent / ".." / "resources")
         file_name = kwargs.get("file_name", "data.json")
-        records = self.generate_records()
+        records = self._generate_records(**kwargs)
+        with open(_path / file_name, "w") as f:
+            json.dump(records, f, indent=4, sort_keys=False)
+
+    @staticmethod
+    def combine_json_files(**kwargs) -> None:
+        """
+        Combine json files.
+        :param kwargs:
+        :return:
+        """
+        _path = kwargs.get("path", Path(__file__).parent / ".." / "resources")
+        file_names = kwargs.get("file_names", ["data_ds_1.json", "data_ds_2.json"])
+        combined_file_name = kwargs.get("combined_file_name", "data_combined.json")
+        records = []
+        for file_name in file_names:
+            with open(_path / file_name, "r") as f:
+                records.extend(json.load(f))
+        with open(_path / combined_file_name, "w") as f:
+            json.dump(records, f, indent=4, sort_keys=False)
+
+    @staticmethod
+    def make_ds_data(**kwargs) -> None:
+        """
+        Make data for drill string. This method is used to generate some
+        dummy data with drillstring id, flowrate, down hole-motor_id, etc.
+        :param kwargs:
+        :return:
+        """
+        _path = kwargs.get("path", Path(__file__).parent / ".." / "resources")
+        file_name = kwargs.get("file_name", "ds_data.json")
+        number_of_ds = kwargs.get("number_of_datapoints", 3)
+        ids: List[str] = [str(uuid.uuid4()) for _ in range(1, number_of_ds + 1)]
+        ds_ids = [f"ds_{i}" for i in range(1, number_of_ds + 1)]
+        flowrate_min: float = kwargs.get("mds_min", 0)
+        flowrate_max: float = kwargs.get("mds_max", 500)
+        flowrates: np.ndarray = np.random.uniform(flowrate_min, flowrate_max, number_of_ds)
+        # use 10 character random striing id for down hole motor.
+        down_hole_motor_ids: List[str] = [str(uuid.uuid4())[:10] for _ in range(1, number_of_ds + 1)]
+        records: List[Dict[str, Any]] = [
+            {
+                "id": id,
+                "drill_string_id": ds_id,
+                "down_hole_motor_id": dhm_id,
+                "flowrate": round(flowrate, 3),
+            }
+                for id, ds_id, dhm_id, flowrate in zip(ids, ds_ids, down_hole_motor_ids, flowrates)
+        ]
         with open(_path / file_name, "w") as f:
             json.dump(records, f, indent=4, sort_keys=False)
 
 
 if __name__ == "__main__":
-
     obj = GenerateData()
-    obj.generate_records()
-    obj.save_data()
+
+    obj.generate_records_and_save_data(
+        file_name="data_ds_1.json",
+        number_of_datapoints=10,
+        drill_string_id="ds_1",
+    )
+
+    obj.generate_records_and_save_data(
+        file_name="data_ds_2.json",
+        number_of_datapoints=10,
+        drill_string_id="ds_2",
+    )
+
+    obj.generate_records_and_save_data(
+        file_name="data_ds_3.json",
+        number_of_datapoints=10,
+        drill_string_id="ds_3",
+    )
+    obj.combine_json_files(
+        combined_file_name="data_combined.json",
+        file_names=["data_ds_1.json", "data_ds_2.json", "data_ds_3.json"]
+    )
+
+    obj.make_ds_data()
