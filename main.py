6309c1edf10941b6672c0f6ff9376b1c6f0fda0d
atashnezhad <atashnezhad1@gmail.com>
2023-04-18 22:57:01 -0500

    automated git @ Tue Apr 18 22:57:01 CDT 2023

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
+
+import pydantic
 from fastapi import FastAPI
-from typing import Dict
+from typing import Dict, Any, Optional
 from src.osu_api import Api
 from src.p03_1_app import BGApp
 
@@ -11,9 +14,11 @@ def home():
     return {"health_check": "OK"}
 
 
-@app.post("/predict")
-def lambda_handler(api: Api, event: Dict, context=None):
-    event = event["body"]
-    obj = BGApp(api, event)
+@app.get("/task/{event}")
+def task(event: str):
+    api = Api()
+    event_dict = json.loads(event)
+    # print(event_dict)
+    obj = BGApp(api, event_dict)
     returned_value = obj.run()
     return returned_value
---
+++ test/test_server.py
@@ -0,0 +1,52 @@
+import json
+
+import pytest
+import requests
+
+
+@pytest.fixture
+def url():
+    url = "http://127.0.0.1:8080"
+    return url
+
+
+# skip this test for now
+@pytest.mark.skip
+def test_server_get(url):
+    url = "http://127.0.0.1:8080"
+    response = requests.get(url)
+    print("\n", response.json())
+    assert response.status_code == 200
+    assert response.json() == {"health_check": "OK"}
+
+
+@pytest.mark.skip
+def test_server_task_return_cache(url):
+    event_dict = {
+        "start_ts": 1677112070,
+        "end_ts": 1677112070 + 60,
+        "asset_id": 123456789,
+        "task": "return_cache",
+    }
+    event_str = json.dumps(event_dict)
+    # print(event_str)
+    url = f"http://127.0.0.1:8080/task/{event_str}"
+    response = requests.get(url)
+    assert response.status_code == 200
+    print("\n", response.json())
+
+
+@pytest.mark.skip
+def test_server_task_get_app_setting(url):
+    event_dict = {
+        "start_ts": 1677112070,
+        "end_ts": 1677112070 + 60,
+        "asset_id": 123456789,
+        "task": "get_app_setting",
+    }
+    event_str = json.dumps(event_dict)
+    # print(event_str)
+    url = f"http://127.0.0.1:8080/task/{event_str}"
+    response = requests.get(url)
+    assert response.status_code == 200
+    print("\n", response.json())
