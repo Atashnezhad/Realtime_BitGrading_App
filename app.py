c47ac40e5fc4b5ac194aae8e445cbb17b84641a2
atashnezhad <atashnezhad1@gmail.com>
2023-08-01 15:17:01 -0500

    some works

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import logging
-
-from flask import Flask
+from flask import Flask, request, jsonify
 from pydantic import BaseModel
 from src.osu_api import Api
 from src.p03_1_app import BGApp
 
 app = Flask(__name__)
-
 
 # Define the Pydantic model for the task payload
 class Event(BaseModel):
@@ -15,21 +13,25 @@ class Event(BaseModel):
     asset_id: int = 123456789
     task: str = "return_cache"
 
-
 @app.route('/')
 def home():
     return {"health_check": "OK"}
 
+@app.route("/task", methods=["POST"])
+def task():
+    try:
+        event_data = request.json
+        event = Event(**event_data)
+    except Exception as e:
+        return jsonify({"error": "Invalid payload format"}), 400
 
-@app.post("/task")
-def task(event: Event):
     logging.info(f"Lambda function executed successfully with event {event}")
     api = Api()
     event_dict = event.dict()
     # print(event_dict)
     obj = BGApp(api, event_dict)
     returned_value = obj.run()
-    return returned_value
+    return jsonify(returned_value)
 
-# here is an example of event in the api
+# here is an example of event in the api - fast api
 # {"start_ts": 1677112070,"end_ts": 1677115068,    "asset_id": 123456789,    "task": "return_cache"}
