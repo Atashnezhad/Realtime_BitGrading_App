eaad3bf2128270203d1c97b866778bdec10cc5fe
atashnezhad <atashnezhad1@gmail.com>
2023-08-01 16:18:05 -0500

    fastapi and docker  working2

--- script_dockers.sh
+++ script_dockers.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-run-app() {
-    uvicorn app.main:app --reload --port 8080
+build_image() {
+    docker build -t rtbg-app .
 }
 
-stop-port() {
-    sudo lsof -t -i tcp:8080 | xargs kill -9
+run_container_fastapi() {
+    docker run --name fastapi-container -p 80:80 -d rtbg-app
 }
 
-# Run the FastAPI application
-if [ "$1" = "run-app" ]; then
-    run-app
-# Stop the process running on port 8080
-elif [ "$1" = "stop-port" ]; then
-    stop-port
-else
-    echo "Invalid command. Usage: ./script.sh [run-app|stop-port]"
-fi
+
